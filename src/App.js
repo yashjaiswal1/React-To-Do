72937c26a17b49884f04cbc8911eaab111ec4060
Yash Jaiswal <yashjaiswal@outlook.com>
2020-04-03 12:02:16 +0530

    App component added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function App(){
+    return(
+        <div>
+        <h2>To Do List:</h2>
+        <form>
+             <input type="checkbox" />
+             <label> Task 1</label><br />
+             
+             <input type="checkbox" />
+             <label> Task 2</label><br />
+
+             <input type="checkbox" />
+             <label> Task 3</label><br />
+
+             <input type="checkbox" />
+             <label> Task 4</label><br />
+        </form>
+        </div>
+    );
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function App(){
-  return(
-    <div>
-      <h1>Test</h1>
-    </div>
-  );
-}
+import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"));
