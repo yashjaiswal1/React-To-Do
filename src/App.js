939e4791e4ac0444c6c936aa21e1edd812c07074
Yash Jaiswal <yashjaiswal@outlook.com>
2020-05-20 14:57:59 +0530

    add TodosData.js for mapping components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react"
-import ToDoItem from "./ToDoItem"
-import "./style.css"
+import React from "react";
+import TodoItem from "./ToDoItem";
+import todosData from "./todosData";
+import "./style.css";
 
-function App(){
-    return(
-        <div className="todo-list">
-            <ToDoItem />
-            <ToDoItem />
-            <ToDoItem />
-        </div>
-    );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+  }
+
+  render() {
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
-export default App
+export default App;
--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,14 +1,13 @@
-import React from "react"
-import "./style.css"
+import React from "react";
+import "./style.css";
 
-function ToDoItem(){
-    return(
-        <div className="todo-item">
-            <input type="checkbox" />
-            <label>Checkbox</label>
-            <br />
-        </div>
-    );
+function TodoItem(props) {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" checked={props.item.completed} />
+      <p>{props.item.text}</p>
+    </div>
+  );
 }
 
-export default ToDoItem
+export default TodoItem;
